65cee6de550dfbcf75b566fceb778aab23604d2f
bbk <2845758942@qq.com>
2023-11-17 15:14:46 +0800

    feat: css reset

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { createApp } from 'vue'
 import store from '@/store/index.ts'
 import './style.css'
+import '@unocss/reset/sanitize/sanitize.css'
+import '@unocss/reset/sanitize/assets.css'
 import App from './App.tsx'
 import 'virtual:uno.css'
 import router from './router.ts'
+
+
 const app = createApp(App)
+
 app.use(store)
 app.use(router)
 app.mount('#app')
