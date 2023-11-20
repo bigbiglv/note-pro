0d58b9e9e6723d9ada0d8b9d0d26d148fdcdc2a0
bbk <2845758942@qq.com>
2023-11-20 16:19:16 +0800

    feat: 代码高亮

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import Markdown from 'vite-plugin-md'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import UnoCSS from 'unocss/vite'
+import hljs from 'highlight.js';
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
@@ -11,7 +12,26 @@ export default defineConfig({
       include: [/\.vue$/, /\.md$/],
     }),
     vueJsx(),
-    Markdown(),
+    Markdown({
+      headEnabled: false,
+      markdownItOptions: {
+        html: false,
+        linkify: true,
+        breaks: true,
+        typographer: true,
+        highlight: function (str, lang) {
+          if (lang && hljs.getLanguage(lang)) {
+            try {
+              return `<pre><code class="hljs">${hljs.highlight(lang, str, true).value}</code></pre>`
+            } catch (__) {}
+          }
+      
+          return ''
+        }
+      },
+      markdownItSetup(md) {
+      },
+    }),
     UnoCSS(),
   ],
   resolve: {
