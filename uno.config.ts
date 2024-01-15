09ac25fa65a2d01700a935715c2c41694c6a3f91
bbk <2845758942@qq.com>
2024-01-15 11:24:22 +0800

    chore: 添加icon

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,18 @@
-import { defineConfig } from 'unocss'
-
+import { defineConfig, presetWebFonts, presetIcons, presetMini, presetAttributify } from 'unocss'
 export default defineConfig({
-  // ...UnoCSS选项
+  presets: [
+    // 基础样式预设
+    presetMini({
+      theme: {
+        colors: {
+          primary: '', // class="text-primary bg-primary"
+        }
+      }
+    }), 
+    // 或者 atomic 预设等
+    // presetAtomic(),
+    presetAttributify(),
+    presetWebFonts(),
+    presetIcons({ /* options */ }),
+  ],
 })
